c87cf5b08bb7cd6631f2663a3b067247d9c812a6
v.bartusevicius <valentinas.bartusevicius@ovoko.com>
2022-12-20 11:26:34 +0200

    Grapql bundle init

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -6,4 +6,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Liip\TestFixturesBundle\LiipTestFixturesBundle::class => ['dev' => true, 'test' => true],
+    Overblog\GraphQLBundle\OverblogGraphQLBundle::class => ['all' => true],
 ];
