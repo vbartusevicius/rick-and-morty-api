7995d83413ad4833c6a89d4baa986dc8b438257a
v.bartusevicius <valentinas.bartusevicius@ovoko.com>
2022-12-20 11:27:01 +0200

    Define grapqhql type, query

--- app/src/GraphQL/Resolver/Users.php
+++ app/src/GraphQL/Resolver/Users.php
@@ -1,6 +1,22 @@
 <?php
 
-class Users
+namespace App\GraphQL\Resolver;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Overblog\GraphQLBundle\Definition\Resolver\QueryInterface;
+use Symfony\Component\Runtime\ResolverInterface;
+
+class Users implements QueryInterface
 {
 
+    public function __construct(private readonly UserRepository $repository)
+    {
+
+    }
+
+    public function __invoke(ResolverInterface $resolver)
+    {
+        return $this->repository->find(1);
+    }
 }
